2c5457ecd75144954879014a0f005c079055d37d
Navid <31672446+sky2464@users.noreply.github.com>
2018-11-29 19:25:10 -0500

    merge

---
+++ server/src/main/java/com/travelfilters/web/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.travelfilters.web.repository;
+
+import com.travelfilters.web.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByIdIn(List<Long> userIds);
+
+    Boolean existsByEmail(String email);
+}
