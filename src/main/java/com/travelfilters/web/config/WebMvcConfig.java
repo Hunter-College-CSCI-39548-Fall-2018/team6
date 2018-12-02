ecc5835dcfb3db6a76b3a97fc33f05330f2d0c67
Adrian Struszczyk <adrianstru@gmail.com>
2018-12-01 19:09:53 -0500

    updated mvc controller to serve react app

--- src/main/java/com/travelfilters/web/config/WebMvcConfig.java
+++ src/main/java/com/travelfilters/web/config/WebMvcConfig.java
@@ -1,8 +1,18 @@
 package com.travelfilters.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -15,4 +25,56 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        ResourceResolver resolver = new ReactResourceResolver();
+        registry.addResourceHandler("/**")
+                .resourceChain(true)
+                .addResolver(resolver);
+    }
+
+    public class ReactResourceResolver implements ResourceResolver {
+        // this is the same directory you are using in package.json
+        private static final String REACT_DIR = "/static/";
+
+        // this is directory inside REACT_DIR for react static files
+        private static final String REACT_STATIC_DIR = "static";
+
+        private Resource index = new ClassPathResource(REACT_DIR + "index.html");
+        private List<String> rootStaticFiles = Arrays.asList("favicon.io",
+                "asset-manifest.json", "manifest.json", "service-worker.js");
+
+        @Override
+        public Resource resolveResource(HttpServletRequest request, String requestPath,
+                                        List<? extends Resource> locations, ResourceResolverChain chain) {
+            return resolve(requestPath, locations);
+        }
+
+        @Override
+        public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
+            Resource resolvedResource = resolve(resourcePath, locations);
+            if (resolvedResource == null) {
+                return null;
+            }
+            try {
+                return resolvedResource.getURL().toString();
+            } catch (IOException e) {
+                return resolvedResource.getFilename();
+            }
+        }
+
+        private Resource resolve(String requestPath, List<? extends Resource> locations) {
+
+            if (requestPath == null) return null;
+
+            if (rootStaticFiles.contains(requestPath)
+                    || requestPath.startsWith(REACT_STATIC_DIR)) {
+                return new ClassPathResource(REACT_DIR + requestPath);
+            } else
+                return index;
+        }
+
+    }
+
 }
