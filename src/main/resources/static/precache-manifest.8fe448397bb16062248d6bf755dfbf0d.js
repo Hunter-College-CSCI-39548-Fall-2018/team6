a392d5272b1530c2287cf2bbbf353380eb6622a1
Adrian Struszczyk <adrianstru@gmail.com>
2018-12-06 03:56:46 -0500

    final final build files

--- src/main/resources/static/precache-manifest.8fe448397bb16062248d6bf755dfbf0d.js
+++ src/main/resources/static/precache-manifest.8fe448397bb16062248d6bf755dfbf0d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "d39d14d88df2672a4092",
-    "url": "/static/js/main.d39d14d8.chunk.js"
+    "revision": "b81b512b98dd43f46f07",
+    "url": "/static/js/main.b81b512b.chunk.js"
   },
   {
     "revision": "d37d64a51c4e8cf154d6",
     "url": "/static/js/1.d37d64a5.chunk.js"
   },
   {
-    "revision": "d39d14d88df2672a4092",
+    "revision": "b81b512b98dd43f46f07",
     "url": "/static/css/main.3e0c8bef.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.d850a966.chunk.css"
   },
   {
-    "revision": "74e3bfdce9501e41bbcd2296b8406088",
+    "revision": "f056410b56d3585659cae046005e19ad",
     "url": "/index.html"
   }
 ];
